6af4abcd004de3d919fbc0733481eec95a2e7601
nbain00 <zink47@gmail.com>
2022-11-16 16:25:37 -0700

    Added models

--- restaurantReviews/admin.py
+++ restaurantReviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Restaurant, Reviewer, Type, Review
 
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Reviewer)
+admin.site.register(Type)
+admin.site.register(Review)
