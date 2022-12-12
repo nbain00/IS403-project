ee5f1ac1c1cc773f8805f4b33b0017632afd00f9
bruyndecker <bruyn.decker@byu.edu>
2022-12-12 12:36:31 -0700

    great stuff (forms.py)

--- restaurantProject/settings.py
+++ restaurantProject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurantReviews.apps.RestaurantreviewsConfig'
+    'restaurantReviews.apps.RestaurantreviewsConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
