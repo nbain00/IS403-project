2f1b8e552c50827d3bff3f6ca0e2e4c14a2751d2
bruyndecker <bruyn.decker@byu.edu>
2022-12-12 17:14:23 -0700

    letstrythisonemoretime

--- restaurantProject/settings.py
+++ restaurantProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://restaurantproject.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://is403-project-production.up.railway.app']
 
 # Application definition
 
